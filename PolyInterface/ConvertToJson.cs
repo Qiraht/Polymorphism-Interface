fd2e0c9c7f6f51f8507629bb5fef8a5bb50621c5
Qiraht <48444430+Qiraht@users.noreply.github.com>
2019-05-21 12:05:44 +0700

    First Spices

---
+++ PolyInterface/ConvertToJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PolyInterface
+{
+    public class ConvertToJson : IConvertObject
+    {
+        public void Convert(Mahasiswa mhs)
+        {
+            string json = JsonConvert.SerializeObject(mhs);
+
+            Console.WriteLine("Hasil Konversi ke json : \n");
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ PolyInterface/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.npm = "18.11.2350";
+            mhs.nama = "Thariq Aulia Akbar";
+            mhs.email = "thariq.2712@students.amikom.ac.id";
+
+            Console.WriteLine("Pilih Format Konversi Data:");
+            Console.WriteLine("1. JSON");
+            Console.WriteLine("2. XML");
+            Console.WriteLine("3. CSV \n");
+
+            Console.WriteLine("Nomor Format Data [1..3]: ");
+            int formatData = Convert.ToInt32(Console.ReadLine());
+
+            IConvertObject convert;
+
+            if (formatData == 1)
+                convert = new ConvertToJson();
+            else if (formatData == 2)
+                convert = new ConvertToXml();
+            else
+                convert = new ConvertToCsv();
+
+            convert.Convert(mhs);
+            Console.ReadKey();
+        }
+    }
+}
